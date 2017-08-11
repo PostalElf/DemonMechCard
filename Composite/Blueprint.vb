b77b35e43e20e739bcfad45510169cf5788a3914
PostalElf <thanthos@gmail.com>
2017-08-11 22:57:13 +0800

    First build

--- Composite/Blueprint.vb
+++ Composite/Blueprint.vb
@@ -6,11 +6,26 @@
     Private SlotsFilled As New List(Of String)
     Private SlotsOptional As New List(Of String)
 
-    Public Shared Function Load(ByVal blueprintName As String) As Blueprint
-        Using sr As New System.IO.StreamReader("data/blueprints.txt")
+    Public Overloads Shared Function Load(ByVal blueprintName As String) As Blueprint
+        Dim raw As Queue(Of String) = SquareBracketLoader("data/blueprints.txt", blueprintName)
+        If raw Is Nothing Then Throw New Exception("Invalid BlueprintName") : Return Nothing
 
-        End Using
+        Dim blueprint As New Blueprint
+        With blueprint
+            While raw.Count > 0
+                Dim ln As String() = raw.Dequeue.Split(":")
+                Dim key As String = ln(0).Trim
+                Dim value As String = ln(1).Trim
+                .Build(key, value)
+            End While
+        End With
+        Return blueprint
     End Function
+    Private Sub Build(ByVal key As String, ByVal value As String)
+        Select Case key
+
+        End Select
+    End Sub
     Public Function Construct() As BodyPart
         Dim bp As New BodyPart
         For Each c In Components
--- Composite/Component.vb
+++ Composite/Component.vb
@@ -8,6 +8,32 @@
     Protected Dodge As Integer
     Protected Damages As New Damages
 
+    Public Shared Function Load(ByVal componentName As String) As Component
+        Dim raw As Queue(Of String) = SquareBracketLoader("data/components.txt", componentName)
+        If raw Is Nothing Then Throw New Exception("Invalid ComponentName") : Return Nothing
+
+        Dim component As New Component
+        With component
+            While raw.Count > 0
+                Dim ln As String() = raw.Dequeue.Split(":")
+                Dim key As String = ln(0).Trim
+                Dim value As String = ln(1).Trim
+                .Build(key, value)
+            End While
+        End With
+        Return component
+    End Function
+    Private Sub Build(ByVal key As String, ByVal value As String)
+        Select Case key
+            Case "Speed" : Speed = CInt(value)
+            Case "Defences"
+            Case "AttackRange"
+            Case "AmmoMax"
+            Case "Accuracy"
+            Case "Dodge"
+
+        End Select
+    End Sub
     Public Sub Merge(ByVal c As Component)
         Speed += c.Speed
         For Each d In Defences
