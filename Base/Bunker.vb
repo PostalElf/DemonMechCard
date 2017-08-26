526f32c56cf527bafbfc79aa173f828d20c11d4b
PostalElf <thanthos@gmail.com>
2017-08-26 13:22:47 +0800

    Added BunkerRoom

--- Base/Bunker.vb
+++ Base/Bunker.vb
@@ -1,4 +1,27 @@
 ﻿Public Class Bunker
     Private Name As String
+    Private _Money As Integer
+    Public ReadOnly Property Money As Integer
+        Get
+            Return _Money
+        End Get
+    End Property
 
+    Private LandExcavated As Integer
+    Private LandUnexcavated As Integer
+    Private ReadOnly Property SpaceFree As Integer
+        Get
+            Return LandExcavated - SpaceUsed
+        End Get
+    End Property
+    Private ReadOnly Property SpaceUsed As Integer
+        Get
+            Dim total As Integer = 0
+            For Each r In Rooms
+                total += r.SpaceUsed
+            Next
+            Return total
+        End Get
+    End Property
+    Private Rooms As New List(Of BunkerRoom)
 End Class
---
+++ Base/BunkerRoom.vb
@@ -0,0 +1,14 @@
+﻿Public Class BunkerRoom
+    Private _Name As String
+    Public ReadOnly Property Name As String
+        Get
+            Return _Name
+        End Get
+    End Property
+    Private _SpaceUsed As Integer
+    Public ReadOnly Property SpaceUsed As Integer
+        Get
+            Return _SpaceUsed
+        End Get
+    End Property
+End Class
